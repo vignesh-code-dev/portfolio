9ff6c996eba136c611f5e0d9abab7a490a64984c
vignesh-code-dev <vickyv.developer@gmail.com>
2025-10-04 09:10:32 +0530

    sdad

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import NavBar from "./Components/NavBar/NavBar";
 import Home from "./Components/Home/Home";
-import About from "./Components/About/About";
 import Projects from "./Components/Projects/Projects";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
@@ -11,7 +10,6 @@ const App = () => {
     <div>
       <NavBar />
       <Home />
-      {/* <About /> */}
       <Skill/>
       <Projects />
       <Contact />
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -16,7 +16,7 @@ const Intro = () => {
             <br /> Currently looking for opportunities to contribute <br />
             and grow in a professional environment.
           </p>
-          <a href="public/VIGNESH Resume.pdf" className="intro-btn" download="VIGNESH Resume.pdf">
+          <a href="public/VIGNESH Resume.pdf" className="intro-btn" download="VIGNESH Resume.pdf" rel="noopener">
             Download CV
           </a>
         </div>
